2ecd62c366c714cf0b4b9b6f641538af8119f5e1
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2025-10-25 23:13:34 +0530

    added create table command

---
+++ sql/setup.sql
@@ -0,0 +1,16 @@
+CREATE TABLE customers (
+    RowNumber INT,
+    CustomerId BIGINT,
+    Surname VARCHAR(50),
+    CreditScore INT,
+    Geography VARCHAR(50),
+    Gender VARCHAR(10),
+    Age INT,
+    Tenure INT,
+    Balance FLOAT,
+    NumOfProducts INT,
+    HasCrCard INT,
+    IsActiveMember INT,
+    EstimatedSalary FLOAT,
+    Exited INT
+);
